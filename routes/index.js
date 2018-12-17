ef6ea78de54e483d93d719fdce52bffc73b47a65
Dillies Benoît <benoit.dillies@ynov.com>
2018-12-17 11:06:19 +0100

    Basic chat

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http').Server(express);
+var io = require('socket.io')(http);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index');
 });
+
+router.get('/favicon.ico', (req, res) => res.status(204));
 
 module.exports = router;
